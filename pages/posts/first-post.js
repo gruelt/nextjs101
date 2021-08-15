c0215f164d28314df5e3d823e5d977a63f3e2739
gruelt <gruelt@gmail.com>
2021-08-16 00:21:08 +0200

    add ing files

---
+++ pages/posts/first-post.js
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Layout from "../../components/layout";
+import {Container, Row, Col} from 'react-bootstrap';
+
+export default function FirstPost() {
+    return (
+        <Layout>
+            <Head>
+                <title>Premier Post!!</title>
+            </Head>
+
+
+
+
+            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+                <div className="md:flex">
+                    <div className="md:flex-shrink-0">
+
+                    </div>
+
+
+
+                    </div>
+                    <div className="p-8">
+                        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
+                        <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding
+                            customers for your new business</a>
+                        <p className="mt-2 text-gray-500">Getting a new business off the ground is a lot of hard work.
+                            Here are five ideas you can use to find your first customers.</p>
+                    </div>
+                </div>
+
+
+
+
+
+
+            </Layout>
+    )
+}
